5b63fe2e531282eab17de59c295390a28bd1cfde
Ilya <ilblackdragon@gmail.com>
2011-04-15 16:08:45 +0300

    Add StripCookieMiddleware

--- misc/middleware.py
+++ misc/middleware.py
@@ -1,3 +1,4 @@
+import re
 from misc.utils import custom_spaceless
 
 class SpacelessMiddleware(object):
@@ -5,3 +6,12 @@ class SpacelessMiddleware(object):
         if 'text/html' in response['Content-Type']:
             response.content = custom_spaceless(response.content)
         return response
+
+class StripCookieMiddleware(object):
+    strip_re = re.compile(r'(__utm.=.+?(?:; |$))')
+    def process_request(self, request):
+        try:
+            cookie = self.strip_re.sub('', request.META['HTTP_COOKIE'])
+            request.META['HTTP_COOKIE'] = cookie
+        except: 
+            pass        

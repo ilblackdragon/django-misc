5935ed40ccb6086e226bb663f0dc4af790c86314
Illia Polosukhin <ilblackdragon@gmail.com>
2012-11-17 23:45:58 -0800

    Release version 0.1 - documentation is partly in place and app does substantial part of utilities that are required in almost any Django project.

--- misc/__init__.py
+++ misc/__init__.py
@@ -1,10 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 
-__version__ = VERSION = '0.0.9'
+__version__ = VERSION = '0.1.0'
 __project__ = PROJECT = 'django-misc'
 
 log = logging.getLogger( __name__ )
-
-
-
